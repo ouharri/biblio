4641bcef0dbe8b16d3cb67de8b867e2cdcec30c1
ouharri <ouharrioutman@gmail.com>
2023-09-21 16:42:55 +0100

    70% 😊

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,4 @@
 module com.biblio {
-    requires javafx.controls;
-    requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-//            requires eu.hansolo.tilesfx;
     requires java.sql;
     requires lombok;
     requires jBCrypt;
